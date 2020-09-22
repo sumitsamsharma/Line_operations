7e47efab7052dbcca88a2df9279ab9fcf84bff4c
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-22 18:50:59 +0530

    Firs

---
+++ Length.java
@@ -0,0 +1,14 @@
+package Line_comparison;
+
+public class Length 
+{
+	public static void main(String[] args)
+	{
+		double x1=Math.floor(Math.random()*10);
+		double y1=Math.floor(Math.random()*10);
+		double x2=Math.floor(Math.random()*10);
+		double y2=Math.floor(Math.random()*10);
+		int length=(int)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		System.out.println("Length is: "+length);
+	}
+}
